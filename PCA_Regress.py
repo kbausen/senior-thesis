f5fa029711fde64b79921b2781eb0c903027ab3b
kbausen <kb6113@princeton.edu>
2026-03-06 15:14:25 -0500

    changed back the mean subtraction in sup_tuning

--- PCA_Regress.py
+++ PCA_Regress.py
@@ -1285,8 +1285,8 @@ def sup_tuning (tensor_N, tensor_M, dims = 6, fig_4D = False):
         X_null = null_tensor[:, :, t]
         
         # subtract across-condition mean
-        X_null -= X_null.mean(axis=0, keepdims=True)
-        X_pot  -= X_pot.mean(axis=0, keepdims=True)
+        X_null = X_null - X_null.mean(axis=0, keepdims=True)
+        X_pot  = X_pot - X_pot.mean(axis=0, keepdims=True)
 
         # squaring and adding values and dividing by condition numbers to compute variance
         V_null[t] = np.sum(X_null**2) / cond
